62c80e0a49d7fcd0b54a1338369c370da65bc19b
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 04:54:56 +0000

    Automatically backed up by Learn

--- lib/customer.rb
+++ lib/customer.rb
@@ -1,3 +1,20 @@
 class Customer
+  attr_accessor :name, :age
+  
+  @@all = []
+  
+  def initialize (name, age)
+    @name = name
+    @age = age
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
   
 end
