136b99398f4523ee8e742ae40ac487b9284bacaa
Justin Pierce <jwpierce1984@gmail.com>
2020-02-05 04:59:12 +0000

    Automatically backed up by Learn

--- lib/waiter.rb
+++ lib/waiter.rb
@@ -1,3 +1,20 @@
 class Waiter
-
+  attr_accessor :name, :yrs_experience
+  
+  @@all = []
+  
+  def initialize (name, yrs_experience)
+    @name = name
+    @yrs_experience = yrs_experience
+    save
+  end
+  
+  def self.all
+    @@all
+  end
+  
+  def save
+    @@all << self
+  end
+  
 end
